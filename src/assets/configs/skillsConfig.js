2c4086d2372eeaff1ab2372ab765add0081db505
AndySeo <andyhmseo@gmail.com>
2025-11-01 13:02:17 -0400

    update

--- src/assets/configs/skillsConfig.js
+++ src/assets/configs/skillsConfig.js
@@ -8,7 +8,6 @@ import {
   SiTableau,
   SiGoogleads,
   SiMeta,
-  SiMicrosoftpowerpoint,
   SiMicrosoftazure,
   SiChartdotjs,
   SiLooker,
@@ -16,6 +15,7 @@ import {
 } from "react-icons/si";
 import { GrMysql } from "react-icons/gr";
 import { FaGitAlt } from "react-icons/fa";
+import { BiBarChartAlt2 } from "react-icons/bi";
 import React from "react";
 import StorageIcon from "@mui/icons-material/Storage";
 
@@ -38,7 +38,7 @@ const skillsConfig = {
     { id: "skills-comp-airflow", className: "skill-icon", icon: <SiMeta size={ICON_SIZE} />, text: "Meta Ads Manager" },
     { id: "skills-comp-js", className: "skill-icon", icon: <SiGoogleads size={ICON_SIZE} />, text: "Google Ads" },
     { id: "skills-comp-css", className: "skill-icon", icon: <SiTableau size={ICON_SIZE} />, text: "Tableau" },
-    { id: "skills-comp-html", className: "skill-icon", icon: <SiMicrosoftpowerpoint size={ICON_SIZE} />, text: "PowerBi" },
+    { id: "skills-comp-html", className: "skill-icon", icon: <BiBarChartAlt2 size={ICON_SIZE} />, text: "PowerBi" },
     { id: "skills-comp-react", className: "skill-icon", icon: <SiLooker size={ICON_SIZE} />, text: "Looker" },
     { id: "skills-comp-kafka", className: "skill-icon", icon: <SiMicrosoftazure size={ICON_SIZE} />, text: "Azure" },
     { id: "skills-comp-powerbi", className: "skill-icon", icon: <SiChartdotjs size={ICON_SIZE} />, text: "Amplitude" }
